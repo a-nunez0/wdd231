b7ec74fb2e38c5712f93d3d1cfe007ed29271476
a-nunez0 <122509183+a-nunez0@users.noreply.github.com>
2025-01-23 22:37:58 -0700

    ok

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+const mainnav = document.querySelector('#menu')
+const hambutton = document.querySelector('.navagation');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
